5c2a03d214677e3ece9527fd9585a27deaf45159
Vlad R <vladislav.rykln@gmal.com>
2021-02-08 00:46:21 +0300

    entity relation 1.0

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  has_one :category
+  has_many :questions
+  has_many :users, through: :user_tests
+  has_one :user
+
   def self.tests_by_category(cat_title)
     Test.joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: cat_title }).pluck(:title)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_203257) do
+ActiveRecord::Schema.define(version: 2021_02_07_211836) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_203257) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
