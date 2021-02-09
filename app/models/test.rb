5dca9b18195150d0940f82e9ce30d484c658b6ee
Vlad R <vladislav.rykln@gmal.com>
2021-02-09 23:00:28 +0300

    entity relation 1.2

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :user_tests
   has_many :users, through: :user_tests
-  belongs_to :author, class_name: "user"
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   def self.tests_by_category(cat_title)
     Test.joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: cat_title }).pluck(:title)
--- db/migrate/20210207211836_add_user_id_to_tests.rb
+++ db/migrate/20210207211836_add_user_id_to_tests.rb
@@ -1,5 +1,5 @@
 class AddUserIdToTests < ActiveRecord::Migration[6.0]
   def change
-    add_reference :tests, :user, index: true, foreign_key: true
+    add_reference :tests, :author, index: true, foreign_key: :user
   end
 end
