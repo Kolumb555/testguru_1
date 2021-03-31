7c8fad826153a2138c134d12f313485b36be7ff3
Vlad R <vladislav.rykln@gmal.com>
2021-03-31 22:37:11 +0300

    authentication 1.0

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+
+  skip_before_action :authenticate_user!
+
+  helper_method :flash_message
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to cookies[:desired_path]
+    
+    else
+      flash_message('Are you a Guru? Verify your Email and Password please')
+      render :new
+    end
+  end
+
+  def flash_message(message)
+    flash[:alert] = message
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to tests_path
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_153430) do
+ActiveRecord::Schema.define(version: 2021_03_26_133542) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_153430) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "questions", "tests"
