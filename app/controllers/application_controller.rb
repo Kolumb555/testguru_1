6109f12a23984913a389b4f6bb11651549b25aa7
Vlad R <vladislav.rykln@gmal.com>
2021-05-09 13:34:50 +0300

    devise 1.4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    if resource.is_a?(Admin)
+    if resource.admin?
       admin_tests_path
     else
       super
     end
-  end
-
-  def admin?
-    self.is_a?(Admin)
   end
 
   private
